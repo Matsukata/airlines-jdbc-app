fbf7e70f9108da27e3146efce586b9f843a173f6
Matsukata <69299734+Matsukata@users.noreply.github.com>
2021-04-23 23:35:39 +0300

    MKAJ-3 add CrewDaoImpl (#4)
    * MKAJ-3 add CrewDaoImpl
    
    * MKAJ-3 refactor CrewMemberDaoImpl
    
    * MKAJ-3 refactor CrewMemberDaoImpl
    
    * MKAJ-3 refactor CrewMemberDaoImpl
    
    * MKAJ-3 refactor AirplaneDao

--- src/main/java/com/airlines/dao/impl/CrewMemberDaoImpl.java
+++ src/main/java/com/airlines/dao/impl/CrewMemberDaoImpl.java
@@ -2,8 +2,9 @@ package com.airlines.dao.impl;
 
 import com.airlines.dao.CrewMemberDao;
 import com.airlines.exception.DaoOperationException;
-import com.airlines.model.Airplane;
+import com.airlines.model.Citizenship;
 import com.airlines.model.CrewMember;
+import com.airlines.model.Position;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -12,9 +13,12 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import static java.lang.String.format;
+
 public class CrewMemberDaoImpl implements CrewMemberDao {
-    private static final String INSERT_QUERY = "INSERT INTO crew_members " +
-            "(first_name, last_name, position, birthday, citizenship) VALUES (?, ?, ?, ?, ?);";
+    private static final String INSERT_QUERY = "INSERT INTO crew_members (first_name, last_name, position, birthday, citizenship) VALUES (?, ?, ?, ?, ?);";
+    private static final String SELECT_BY_ID_QUERY = "SELECT * FROM crew_members WHERE id = ?;";
+    private static final String UPDATE_QUERY = "UPDATE crew_members SET first_name = ?, last_name = ?, position = ?, birthday = ?, citizenship = ? WHERE id = ?;";
 
     private DataSource dataSource;
 
@@ -23,38 +27,66 @@ public class CrewMemberDaoImpl implements CrewMemberDao {
     }
 
     @Override
-    public void save(CrewMember crew) {
+    public void save(CrewMember crewMember) {
         try (Connection connection = dataSource.getConnection();
              PreparedStatement insertStatement = connection.prepareStatement(INSERT_QUERY,
                      PreparedStatement.RETURN_GENERATED_KEYS);) {
-            insertStatement.setString(1, crew.getFirstName());
-            insertStatement.setString(2, crew.getLastName());
-            insertStatement.setObject(3, crew.getPosition());
-            insertStatement.setDate(4, Date.valueOf(crew.getBirthday()));
-            insertStatement.setObject(5, crew.getCitizenship());
+            insertStatement.setString(1, crewMember.getFirstName());
+            insertStatement.setString(2, crewMember.getLastName());
+            insertStatement.setObject(3, crewMember.getPosition());
+            insertStatement.setDate(4, Date.valueOf(crewMember.getBirthday()));
+            insertStatement.setObject(5, crewMember.getCitizenship());
             insertStatement.executeUpdate();
             ResultSet generatedKeys = insertStatement.getGeneratedKeys();
             if (generatedKeys.next()) {
                 long id = generatedKeys.getLong(1);
-                crew.setId(id);
+                crewMember.setId(id);
             }
         } catch (SQLException e) {
-            throw new DaoOperationException("Error saving crew: " + crew, e);
+            throw new DaoOperationException("Could not save crewMember: " + crewMember, e);
         }
     }
 
     @Override
-    public CrewMember findOne(Long id) {
-        return null;
-    }
-
-    @Override
-    public CrewMember link(Airplane airplane, Long id) {
-        return null;
+    public CrewMember findById(Long id) {
+        if (id == null) {
+            throw new DaoOperationException("Cannot find the member because Id is not provided");
+        }
+        CrewMember crewMember = new CrewMember();
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement selectByIdStatement = connection.prepareStatement(SELECT_BY_ID_QUERY);) {
+            selectByIdStatement.setLong(1, id);
+            ResultSet resultSet = selectByIdStatement.executeQuery();
+            if (resultSet.next()) {
+                crewMember.setId(resultSet.getLong("id"));
+                crewMember.setFirstName(resultSet.getString("first_name"));
+                crewMember.setLastName(resultSet.getString("last_name"));
+                crewMember.setPosition(Position.valueOf(resultSet.getString("position")));
+                crewMember.setCitizenship(Citizenship.valueOf(resultSet.getString("citizenship")));
+            }
+        } catch (SQLException e) {
+            throw new DaoOperationException(format("Crew member with id = %d was not found", id), e);
+        }
+        return crewMember;
     }
 
     @Override
     public CrewMember update(CrewMember crewMember) {
-        return null;
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(UPDATE_QUERY)) {
+            if (crewMember.getId() == null) {
+                throw new IllegalArgumentException("CrewMember id should not be null");
+            }
+            statement.setString(1, crewMember.getFirstName());
+            statement.setString(2, crewMember.getLastName());
+            statement.setString(3, crewMember.getPosition().toString());
+            statement.setDate(4, Date.valueOf(crewMember.getBirthday()));
+            statement.setString(5, crewMember.getCitizenship().toString());
+            statement.setLong(6, crewMember.getId());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new DaoOperationException(format("Can not update a crew member with id = %d", crewMember.getId()), e);
+        }
+        return crewMember;
     }
 }
--- src/main/java/com/airlines/model/CrewMember.java
+++ src/main/java/com/airlines/model/CrewMember.java
@@ -62,18 +62,40 @@ public class CrewMember {
         return citizenship;
     }
 
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public void setCitizenship(Citizenship citizenship) {
+        this.citizenship = citizenship;
+    }
+
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
 
         CrewMember crew = (CrewMember) o;
 
-        if (id != null ? !id.equals(crew.id) : crew.id != null) return false;
-        if (firstName != null ? !firstName.equals(crew.firstName) : crew.firstName != null) return false;
-        if (lastName != null ? !lastName.equals(crew.lastName) : crew.lastName != null) return false;
-        if (position != null ? !position.equals(crew.position) : crew.position != null) return false;
-        if (birthday != null ? !birthday.equals(crew.birthday) : crew.birthday != null) return false;
+        if (id != null ? !id.equals(crew.id) : crew.id != null) {
+            return false;
+        }
+        if (firstName != null ? !firstName.equals(crew.firstName) : crew.firstName != null) {
+            return false;
+        }
+        if (lastName != null ? !lastName.equals(crew.lastName) : crew.lastName != null) {
+            return false;
+        }
+        if (position != null ? !position.equals(crew.position) : crew.position != null) {
+            return false;
+        }
+        if (birthday != null ? !birthday.equals(crew.birthday) : crew.birthday != null) {
+            return false;
+        }
         return citizenship != null ? citizenship.equals(crew.citizenship) : crew.citizenship == null;
     }
 
