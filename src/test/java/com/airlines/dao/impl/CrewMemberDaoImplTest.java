d62b7cb16d77d438e813a354ca9b0934c91baa36
Alexandra Simonova <mazukata@gmail.com>
2021-05-06 11:34:24 +0300

    MKAJ-7 cover CrewMemberDaoImpl with unit tests
    Tech notes:
     - add dependencies to pom.xml (H2 database driver and Junit 4.13)
     - run all tests and confirm they are all green

--- src/test/java/com/airlines/dao/impl/CrewMemberDaoImplTest.java
+++ src/test/java/com/airlines/dao/impl/CrewMemberDaoImplTest.java
@@ -1,11 +1,11 @@
 package com.airlines.dao.impl;
 
 import com.airlines.dao.CrewMemberDao;
-import com.airlines.model.Citizenship;
 import com.airlines.model.CrewMember;
-import com.airlines.model.Position;
 import com.airlines.util.JdbcUtil;
 import com.airlines.util.TestDatasourceProvider;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import javax.sql.DataSource;
@@ -13,75 +13,136 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDate;
+import java.util.Optional;
 
+import static com.airlines.model.Citizenship.BRAZILIAN;
+import static com.airlines.model.Citizenship.ITALIAN;
+import static com.airlines.model.Citizenship.NORWEGIAN;
+import static com.airlines.model.Position.AIRCRAFT_PILOT;
+import static com.airlines.model.Position.FIRST_OFFICER;
+import static com.airlines.model.Position.FLIGHT_ATTENDANT;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
 
 public class CrewMemberDaoImplTest {
-    private static final String CREATE_SCHEMA = JdbcUtil.getSqlQueryString("C:\\Users\\Aleksandra\\IdeaProjects\\airlines-jdbc-app\\src\\main\\resources\\db\\schema.sql");
-    private static final String CREATE_TEST_DATA = JdbcUtil.getSqlQueryString("C:\\Users\\Aleksandra\\IdeaProjects\\airlines-jdbc-app\\src\\main\\resources\\db\\test_data.sql");
-    DataSource dataSource = TestDatasourceProvider.createTestDatasource();
-    CrewMemberDao crewMemberDao = new CrewMemberDaoImpl(dataSource);
+    private static final String SCHEMA_SQL_PATH = "src/test/resources/sql/schema.sql";
+    private static final String DATA_SQL_PATH = "src/test/resources/sql/test_data.sql";
+    private static final String CREATE_TABLES_QUERY = JdbcUtil.getSqlQueryString(SCHEMA_SQL_PATH);
+    private static final String GENERATE_DATA_QUERY = JdbcUtil.getSqlQueryString(DATA_SQL_PATH);
+    private static final CrewMember CREW_MEMBER = buildTestCrewMember();
+    private static final CrewMember TEST_DATA_CREW_MEMBER = buildCrewMemberFromDataset();
 
-    @Test
-    public void save() {
-        try (Connection connection = dataSource.getConnection()) {
-            Statement createTableStatement = connection.createStatement();
-            createTableStatement.executeUpdate(CREATE_SCHEMA);
-            createTableStatement.executeUpdate(CREATE_TEST_DATA);
+    private static CrewMemberDao crewMemberDao;
 
-            CrewMember crewMember = generateCrewMember();
-            CrewMember newCrewMember = crewMemberDao.save(crewMember).get();
-            CrewMember crewMemberById = crewMemberDao.findById(11L).get();
+    private static DataSource dataSource;
 
-            assertEquals(newCrewMember, crewMemberById);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
+    @BeforeClass
+    public static void init() {
+        dataSource = TestDatasourceProvider.createTestDatasource();
+        crewMemberDao = new CrewMemberDaoImpl(dataSource);
+    }
+
+    @Before
+    public void dataBaseInit() throws SQLException {
+        createTables();
     }
 
     @Test
-    public void findById() {
-        try (Connection connection = dataSource.getConnection()) {
-            Statement createTableStatement = connection.createStatement();
-            createTableStatement.executeUpdate(CREATE_SCHEMA);
-            createTableStatement.executeUpdate(CREATE_TEST_DATA);
+    public void shouldFindCorrectCrewMember() throws SQLException {
+        populateTestData();
+        Optional<CrewMember> crewMember = crewMemberDao.findById(TEST_DATA_CREW_MEMBER.getId());
 
-            CrewMember crewMember = generateCrewMember();
-            CrewMember newCrewMember = crewMemberDao.save(crewMember).get();
-            CrewMember crewMemberById = crewMemberDao.findById(11L).get();
-
-            assertEquals(newCrewMember.getId(), crewMemberById.getId());
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
+        assertTrue(crewMember.isPresent());
+        CrewMember actual = crewMember.get();
+        assertEquals(TEST_DATA_CREW_MEMBER, actual);
     }
 
     @Test
-    public void update() {
-        try (Connection connection = dataSource.getConnection()) {
-            Statement createTableStatement = connection.createStatement();
-            createTableStatement.executeUpdate(CREATE_SCHEMA);
-            createTableStatement.executeUpdate(CREATE_TEST_DATA);
-
-            CrewMember testCrewMember = generateCrewMember();
-            CrewMember testSaveCrewMember = crewMemberDao.save(testCrewMember).get();
-
-            CrewMember newCrewMember = crewMemberDao.save(testCrewMember).get();
-            crewMemberDao.update(newCrewMember);
-
-            assertEquals(testSaveCrewMember, newCrewMember);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
+    public void shouldReturnEmptyOptionalIfCrewMemberWasNotFound() throws SQLException {
+        populateTestData();
+        Optional<CrewMember> crewMember = crewMemberDao.findById(15L);
+        assertTrue(crewMember.isEmpty());
     }
 
-    private CrewMember generateCrewMember() {
+    @Test
+    public void shouldThrowExceptionIfInvalidArgumentProvidedToFindByIdMethod() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> crewMemberDao.findById(null));
+        assertEquals("Cannot find the member because id is not provided", exception.getMessage());
+    }
+
+    @Test
+    public void shouldCorrectlySaveCrewMemberToDatabase() {
+        Optional<CrewMember> savedCrewMember = crewMemberDao.save(CREW_MEMBER);
+        assertTrue(savedCrewMember.isPresent());
+        assertEquals(CREW_MEMBER, savedCrewMember.get());
+
+        CrewMember foundEntity = crewMemberDao.findById(CREW_MEMBER.getId()).get();
+        assertEquals(CREW_MEMBER, foundEntity);
+    }
+
+    @Test
+    public void shouldUpdateCrewMember() throws SQLException {
+        populateTestData();
+        CrewMember expected = CrewMember.builder()
+                .withId(1L)
+                .withFirstName("Ivanna")
+                .withLastName("Armstrong")
+                .withCitizenship(BRAZILIAN)
+                .withPosition(FLIGHT_ATTENDANT)
+                .withBirthday(LocalDate.of(1990, 12, 7))
+                .build();
+
+        crewMemberDao.update(expected);
+
+        Optional<CrewMember> result = crewMemberDao.findById(1L);
+
+        assertTrue(result.isPresent());
+        assertEquals(expected, result.get());
+    }
+
+    @Test
+    public void shouldThrowExceptionIfIdIsNotProvidedForUpdateMethod() {
+        CrewMember crewMember = CrewMember.builder().build();
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> crewMemberDao.update(crewMember));
+        assertEquals("CrewMember id should not be null", exception.getMessage());
+    }
+
+    private static CrewMember buildTestCrewMember() {
         return CrewMember.builder()
-                .withFirstName("Mary")
-                .withLastName("Johnes")
-                .withPosition(Position.FLIGHT_ENGINEER)
-                .withBirthday(LocalDate.of(2000, 4, 23))
-                .withCitizenship(Citizenship.ITALIAN)
+                .withId(1L)
+                .withFirstName("John")
+                .withLastName("Smith")
+                .withCitizenship(ITALIAN)
+                .withPosition(AIRCRAFT_PILOT)
+                .withBirthday(LocalDate.of(2002, 10, 5))
                 .build();
     }
+
+    private static CrewMember buildCrewMemberFromDataset() {
+        return CrewMember.builder()
+                .withId(7L)
+                .withFirstName("Marat")
+                .withLastName("Abakulov")
+                .withCitizenship(NORWEGIAN)
+                .withPosition(FIRST_OFFICER)
+                .withBirthday(LocalDate.of(1994, 3, 8))
+                .build();
+    }
+
+    private static void createTables() throws SQLException {
+        executeQuery(dataSource, CREATE_TABLES_QUERY);
+    }
+
+    private static void populateTestData() throws SQLException {
+        executeQuery(dataSource, GENERATE_DATA_QUERY);
+    }
+
+    private static void executeQuery(DataSource dataSource, String query) throws SQLException {
+        try (Connection connection = dataSource.getConnection()) {
+            Statement createTableStatement = connection.createStatement();
+            createTableStatement.execute(query);
+        }
+    }
+
 }
---
+++ src/test/resources/sql/schema.sql
@@ -0,0 +1,41 @@
+DROP TABLE IF EXISTS crews_crew_members;
+DROP TABLE IF EXISTS crew_members;
+DROP TABLE IF EXISTS airplanes;
+DROP TABLE IF EXISTS crews;
+
+CREATE TABLE crews
+(
+    id   INT PRIMARY KEY,
+    name VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE airplanes
+(
+    id               SERIAL PRIMARY KEY,
+    code_name        VARCHAR(10) NOT NULL,
+    model            VARCHAR(64) NOT NULL,
+    manufacture_date DATE        NOT NULL,
+    capacity         INT         NOT NULL,
+    flight_range     INT         NOT NULL,
+    crew_id          INT,
+    CONSTRAINT airplanes_crews_FK FOREIGN KEY (crew_id) REFERENCES crews (id)
+);
+
+CREATE TABLE crew_members
+(
+    id          SERIAL PRIMARY KEY,
+    first_name  VARCHAR(100) NOT NULL,
+    last_name   VARCHAR(100) NOT NULL,
+    position    VARCHAR(20)  NOT NULL,
+    birthday    DATE         NOT NULL,
+    citizenship VARCHAR(20)  NOT NULL
+);
+
+CREATE TABLE crews_crew_members
+(
+    crew_id        BIGINT NOT NULL,
+    crew_member_id BIGINT NOT NULL,
+    CONSTRAINT PK_crews_crew_members PRIMARY KEY (crew_id, crew_member_id),
+    CONSTRAINT FK_crews_crew_members FOREIGN KEY (crew_id) REFERENCES crews (id),
+    CONSTRAINT FK_crew_members_crews FOREIGN KEY (crew_member_id) REFERENCES crew_members (id)
+);
